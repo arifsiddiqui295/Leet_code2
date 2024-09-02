b6bf3cc72a1e10af19b27a54f5135b50e88cb525
arifsiddiqui295 <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-03 00:38:16 +0530

    Time: 20 ms (11.99%), Space: 44.5 MB (38.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,25 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-        for(int i=0;i<s.length();i++){
-            int c = s.charAt(i);
-            arr1[c-97]++;
+    public boolean isAnagram(String s1, String s2) {
+         if (s1.length() != s2.length()) return false;
+
+        Map<Character, Integer> mp1 = new HashMap<>();
+        Map<Character, Integer> mp2 = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (!mp1.containsKey(s1.charAt(i))) {
+                mp1.put(s1.charAt(i), 1);
+            } else {
+                mp1.put(s1.charAt(i), mp1.get(s1.charAt(i)) + 1);
+            }
         }
-        for(int i=0;i<t.length();i++){
-            int c = t.charAt(i);
-            arr2[c-97]++;
+
+        for (int i = 0; i < s2.length(); i++) {
+            if (!mp2.containsKey(s2.charAt(i))) {
+                mp2.put(s2.charAt(i), 1);
+            } else {
+                mp2.put(s2.charAt(i), mp2.get(s2.charAt(i)) + 1);
+            }
         }
-        for(int i =0 ; i<arr1.length;i++){
-            if(arr1[i]!=arr2[i]) return false;
-        }
-        return true;
+        return mp1.equals(mp2);
     }
 }
