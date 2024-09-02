c814dbfa12c513a6aa592b5a075d19c3df9197a7
arifsiddiqui295 <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-03 00:45:42 +0530

    Time: 20 ms (11.99%), Space: 44.5 MB (38.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isAnagram(String s1, String s2) {
-         if (s1.length() != s2.length()) return false;
+       
+        if (s1.length() != s2.length()) return false;
 
         Map<Character, Integer> mp1 = new HashMap<>();
-        Map<Character, Integer> mp2 = new HashMap<>();
 
         for (int i = 0; i < s1.length(); i++) {
             if (!mp1.containsKey(s1.charAt(i))) {
@@ -12,14 +12,15 @@ class Solution {
                 mp1.put(s1.charAt(i), mp1.get(s1.charAt(i)) + 1);
             }
         }
-
-        for (int i = 0; i < s2.length(); i++) {
-            if (!mp2.containsKey(s2.charAt(i))) {
-                mp2.put(s2.charAt(i), 1);
-            } else {
-                mp2.put(s2.charAt(i), mp2.get(s2.charAt(i)) + 1);
-            }
+        for(int i = 0 ;i<s2.length();i++){
+            if(!mp1.containsKey(s2.charAt(i))) return false;
+            mp1.put(s2.charAt(i),mp1.get(s2.charAt(i))-1);
         }
-        return mp1.equals(mp2);
+        
+        for(var e : mp1.entrySet()){
+            if(e.getValue()!=0) return false;
+        }
+        
+        return true;
     }
 }
