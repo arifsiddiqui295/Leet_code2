d0aae553e7512983f1589b8933ae53a3baac5ba9
arifsiddiqui295 <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-12 00:03:59 +0530

    Time: 1 ms (78.42%), Space: 45.6 MB (52.2%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,25 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rowSet = new HashSet<>();
-        Set<Integer> colSet = new HashSet<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        boolean[] arr = new boolean[row];
+        boolean[] brr = new boolean[col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 if (matrix[i][j] == 0) {
-                    rowSet.add(i);
-                    colSet.add(j);
+                    arr[i] = true;
+                    brr[j] = true;
                 }
             }
         }
 
-        for (var i : rowSet) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-        for (var j : colSet) {
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][j] = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if(arr[i]||brr[j]){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
