1cf052020d88582169206aa3ad28f0924410eb0c
arifsiddiqui295 <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-14 23:14:18 +0530

    Time: 4 ms (84.66%), Space: 45.1 MB (6.66%) - LeetHub

--- 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.java
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.java
@@ -1,22 +1,17 @@
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
         int sum = 0;
-        int row = -1, column = -1;
-        for (int k = 0; k < grid[0].length; k++) {
-            int max2 = Integer.MIN_VALUE;
-            for (int i = 0; i < grid.length; i++) {
-                int max = Integer.MIN_VALUE;
-                for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] > max) {
-                        max = grid[i][j];
-                        row = i;
-                        column = j;
-                    }
-                }
-                grid[row][column] = -1;
-                max2 = Math.max(max2, max);
+
+        for (int[] row : grid) {
+            Arrays.sort(row);
+        }
+
+        for (int i = grid[0].length-1; i >= 0; i--) {
+            int max = 1;
+            for (int[] row : grid) {
+                max = Math.max(row[i], max);
             }
-            sum = sum + max2;
+            sum = sum + max;
         }
         return sum;
     }
