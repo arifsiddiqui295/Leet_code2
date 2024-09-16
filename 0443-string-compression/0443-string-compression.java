6bfd079dd2b5b7d81ec8c5ab1289409ffbc57007
arifsiddiqui295 <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-16 19:03:08 +0530

    Time: 1 ms (99.57%), Space: 43.8 MB (79.62%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -3,32 +3,29 @@ class Solution {
         int count = 0;
         int index = 0;
         char lastVisited = '\0';
-
         for (int i = 0; i < chars.length; i++) {
             if (count == 0) {
                 lastVisited = chars[i];
                 count = 1;
-            } else if (chars[i] == lastVisited) {
+            } else if (lastVisited == chars[i]) {
                 count++;
             } else {
                 chars[index++] = lastVisited;
                 if (count > 1) {
-                    for (char c : Integer.toString(count).toCharArray()) {
-                        chars[index++] = c;
+                    for (char e : Integer.toString(count).toCharArray()) {
+                        chars[index++] = e;
                     }
                 }
                 lastVisited = chars[i];
                 count = 1;
             }
         }
-
         chars[index++] = lastVisited;
         if (count > 1) {
-            for (char c : Integer.toString(count).toCharArray()) {
-                chars[index++] = c;
+            for (char e : Integer.toString(count).toCharArray()) {
+                chars[index++] = e;
             }
         }
-
         return index;
     }
 }
