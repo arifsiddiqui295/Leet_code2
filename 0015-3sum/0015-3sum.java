6a14048d27e820fdf45c0b34bbdc0f4764e42df8
Arif Siddiqui <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-26 04:14:43 +0530

    Time: 28 ms (79.02%), Space: 52.6 MB (19.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        Set<List<Integer>> ansSet = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < n; i++) {
-            Set<Integer> set = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                List<Integer> list = new ArrayList<>();
-                int sum = -(nums[i] + nums[j]);
-                if (set.contains(sum)) {
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(sum);
-                    Collections.sort(list);
-                }
-                set.add(nums[j]);
-                if (!list.isEmpty()) {
-                    ansSet.add(list);
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
                 }
             }
         }
-        List<List<Integer>> al = new ArrayList<>();
-        al.addAll(ansSet);
-        return al;
+        return ans;
     }
 }
