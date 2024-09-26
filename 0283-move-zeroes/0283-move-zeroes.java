6efaa4bad752c8f9a0c096c73cc8d9dc69ae7b4c
Arif Siddiqui <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-26 17:26:58 +0530

    Time: 3 ms (24.2%), Space: 45.8 MB (50.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int res = 0;
+        if (nums.length < 2)
+            return;
+        int zeroP = -1;
+        int first = -2;
         for (int i = 0; i < nums.length; i++) {
-             if(nums[i]!=0){
-                int temp = nums[res];
-                nums[res]= nums[i];
-                nums[i]=temp;
-                res++;
-             }
+            if (nums[i] == 0 && first == -2) {
+                zeroP = i;
+                first = 1;
+            } else if (nums[i] != 0&&first==1) {
+                int temp = nums[i];
+                nums[i] = nums[zeroP];
+                nums[zeroP] = temp;
+                zeroP++;
+            }
         }
     }
 }
