67216a618469265475797894e1c47dd51de34aa2
Arif Siddiqui <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-26 16:01:17 +0530

    Time: 2 ms (6.42%), Space: 40.8 MB (92.2%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,27 @@
 class Solution {
     public boolean check(int[] nums) {
-        int j = nums.length - 1;
-        boolean check = true;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                check = false;
-            }
-        }
-        if(check ==true) return true;
-        while (j > 0) {
-            boolean isSorted = true;
-            int x = nums[nums.length - 1];
-            for (int i = nums.length - 1; i > 0; i--) {
-                nums[i] = nums[i - 1];
-            }
-            nums[0] = x;
-            j--;
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    isSorted = false;
-                }
-            }
-            if (isSorted == true)
+        int i = 0;
+        while (i < nums.length) {
+            if (isSorted(nums)) {
                 return true;
+            } else {
+                int temp = nums[nums.length - 1];
+                for (int j = nums.length - 1; j > 0; j--) {
+                    nums[j] = nums[j - 1];
+                }
+                nums[0] = temp;
+            }
+            i++;
         }
         return false;
+    }
+
+    public boolean isSorted(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
