62dadf1a9e62f86b3b41907211ddeacf7fc2fa60
Arif Siddiqui <118365626+arifsiddiqui295@users.noreply.github.com>
2024-09-27 15:04:52 +0530

    Time: 0 ms (100%), Space: 44.7 MB (96.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int maximum = Integer.MIN_VALUE;
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             sum = sum + nums[i];
