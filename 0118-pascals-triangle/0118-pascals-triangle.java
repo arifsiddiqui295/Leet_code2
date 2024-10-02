bf8d65c3025c0805798b0bd2ada96289e21a2686
Arif Siddiqui <118365626+arifsiddiqui295@users.noreply.github.com>
2024-10-03 03:43:49 +0530

    Time: 1 ms (84.09%), Space: 41.6 MB (90.69%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,39 +1,17 @@
 class Solution {
-    public List<List<Integer>> generate(int n) {
-        // List<List<Integer>> ans = new ArrayList<>();
-
-        // for (int i = 0; i < n; i++) {
-        // List<Integer> row = new ArrayList<>();
-        // row.add(1);
-
-        // for (int j = 1; j < i; j++) {
-        // row.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
-        // }
-        // if (i > 0) {
-        // row.add(1);
-        // }
-        // ans.add(row);
-        // }
-        // return ans;
-
-        int[][] ans = new int[n][];
-
-        for (int i = 0; i < n; i++) {
-            ans[i] = new int[i + 1];
-            ans[i][0] = ans[i][i] = 1;
-            for (int j = 1; j < i; j++) {
-                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
-            }
-        }
-
-        List<List<Integer>> al = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < ans[i].length; j++) {
-                row.add(ans[i][j]);
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
             }
-            al.add(row);
+            if (i > 0) {
+                row.add(1);
+            }
+            ans.add(row);
         }
-        return al;
+        return ans;
     }
 }
