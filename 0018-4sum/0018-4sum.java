3f12fb7a01ac8f8aa09d2f03a6f1b955650d9911
Arif Siddiqui <118365626+arifsiddiqui295@users.noreply.github.com>
2024-10-18 05:59:41 +0530

    Time: 16 ms (81.41%), Space: 43.7 MB (72.32%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,24 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
         int n = nums.length;
         Set<List<Integer>> res = new HashSet<>();
         for (int i = 0; i < n; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
             for (int j = i + 1; j < n; j++) {
-                Set<Long> temp = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
+                if (j != i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
                     long sum = nums[i] + nums[j];
                     sum = sum + nums[k];
-                    long fourth = target - (sum);
-                    if (temp.contains(fourth)) {
+                    sum = sum + nums[l];
+                    if (sum < target) {
+                        k++;
+                    } else if (sum > target) {
+                        l--;
+                    } else {
                         List<Integer> temp1 = new ArrayList<>();
                         temp1.add(nums[i]);
                         temp1.add(nums[j]);
                         temp1.add(nums[k]);
-                        temp1.add((int) fourth);
-                        Collections.sort(temp1);
+                        temp1.add(nums[l]);
                         res.add(temp1);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
                     }
-                    temp.add((long) nums[k]);
                 }
             }
         }
