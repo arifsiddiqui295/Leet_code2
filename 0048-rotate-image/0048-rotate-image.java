a72ffdf0a6cb615d1191e5548f4a5606cc4fdadd
Arif Siddiqui <118365626+arifsiddiqui295@users.noreply.github.com>
2024-12-13 15:00:59 +0530

    Time: 0 ms (100%), Space: 42.6 MB (6.97%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,23 +1,22 @@
 class Solution {
-    public void rotate(int[][] arr) {
-        transpose(arr);
-        for (int i = 0; i < arr.length; i++) {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        for (int i = 0; i < matrix.length; i++) {
             int start = 0;
-            int end = arr[i].length - 1;
+            int end = matrix[i].length - 1;
             while (start < end) {
-                int temp = arr[i][start];
-                arr[i][start] = arr[i][end];
-                arr[i][end] = temp;
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
                 start++;
                 end--;
             }
         }
     }
-
-    static void transpose(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (i < j) {
+    public static void transpose(int[][]arr){
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
+                if(i<j){
                     int temp = arr[i][j];
                     arr[i][j] = arr[j][i];
                     arr[j][i] = temp;
